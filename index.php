ed59f6d467ef2a62771d6967115064fbc1360a24
Jorge Cisneros <fraxito@gmail.com>
2016-05-20 17:47:08 +0200

    con la funcion que cambia los colores de los botones

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ and open the template in the editor.
                 <div class="col-md-6">
                     <button id="enunciado" class="btn btn-block btn-warning disabled"></button>
                     <br><br>
-                    <button id="r1" class="btn btn-block btn-primary " onclick="chequeaRespuesta();"></button> 
+                    <button id="r1" class="btn btn-block btn-primary " ></button> 
                     <br><br>
-                    <button id="r2" class="btn btn-block btn-primary " onclick="chequeaRespuesta();"></button> 
+                    <button id="r2" class="btn btn-block btn-primary " ></button> 
                     <br><br>
-                    <button id="r3" class="btn btn-block btn-primary " onclick="chequeaRespuesta();"></button> 
+                    <button id="r3" class="btn btn-block btn-primary " ></button> 
                     <br><br>                                                            
-                    <button id="r4" class="btn btn-block btn-primary " onclick="chequeaRespuesta();"></button> 
+                    <button id="r4" class="btn btn-block btn-primary " ></button> 
                 </div>
                 <div class="col-md-3"></div>
             </div>
@@ -73,21 +73,45 @@ and open the template in the editor.
         <script src="js/bootstrap.min.js"></script>
         <script>
         var arrayPreguntas;
-        
-        //desordena una array
+        var listaRespuestas = [3,4,5,6];
+        var pregunta;
+        //desordena un array
         function desordena(o){ //v1.0
             for(var ja, xa, ia = o.length; ia; ja = Math.floor(Math.random() * ia), xa = o[--ia], o[ia] = o[ja], o[ja] = xa);
             return o;
         };
 
+        function comprobarRespuesta( n1, boton){
+            if (arrayPreguntas[pregunta][7] == (listaRespuestas[n1]-2)){
+                boton.removeClass("btn-primary").addClass("btn-success");
+            }
+            else{
+                boton.removeClass("btn-primary").addClass("btn-danger");
+            }
+        }
+
         function cambiaPregunta(){
-            var pregunta = Math.floor(Math.random() * <?php echo sizeof($listaPreguntas);?>); 
+            pregunta = Math.floor(Math.random() * <?php echo sizeof($listaPreguntas);?>); 
             $('#enunciado').html(arrayPreguntas[pregunta][2]);
-            $('#r1').html(arrayPreguntas[pregunta][3]);
-            $('#r2').html(arrayPreguntas[pregunta][4]);
-            $('#r3').html(arrayPreguntas[pregunta][5]);
-            $('#r4').html(arrayPreguntas[pregunta][6]);
-        }    
+            
+            listaRespuestas = desordena(listaRespuestas);
+            $('#r1').html(arrayPreguntas[pregunta][listaRespuestas[0]])
+                    .click(function(){
+                        comprobarRespuesta(0, $(this));
+                    });
+            $('#r2').html(arrayPreguntas[pregunta][listaRespuestas[1]])
+                    .click(function(){
+                        comprobarRespuesta(1, $(this));
+                    });
+            $('#r3').html(arrayPreguntas[pregunta][listaRespuestas[2]])
+                    .click(function(){
+                        comprobarRespuesta(2, $(this));
+                    });
+            $('#r4').html(arrayPreguntas[pregunta][listaRespuestas[3]])
+                    .click(function(){
+                        comprobarRespuesta(3, $(this));
+                    });       
+}    
             
             
         $(document).ready(function(){
